94208dd91529347171054dfe7e1ba7a8b46ab2ac
Karim Amirov <karamirov@gmail.com>
2026-03-09 22:18:53 +0500

    feat: GameBoard with grid and tile spawning

---
+++ Assets/Game/Core/BoardTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class BoardTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            var board = new GameBoard();
+            board.SpawnRandomTile();
+            board.SpawnRandomTile();
+            board.SpawnRandomTile();
+            board.PrintBoard();
+        }
+    }
+}
---
+++ Assets/Game/Core/GameBoard.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Core
+{
+    public class GameBoard
+    {
+        private int [,] _cells = new int[4,4];
+
+        private bool HasEmptyCell()
+        {
+            for (int i = 0; i < _cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < _cells.GetLength(1); j++)
+                {
+                    if (_cells[i, j] == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private List<(int row, int col)> GetEmptyCells()
+        {
+            var emptyCells = new List<(int row, int col)>();
+
+            for (int i = 0; i < _cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < _cells.GetLength(1); j++)
+                {
+                    if (_cells[i, j] == 0)
+                    {
+                        emptyCells.Add((i, j));
+                    }
+                }
+            }
+            return emptyCells;
+        }
+
+        public void SpawnRandomTile()
+        {
+            List<(int row, int col)> emptyCells = GetEmptyCells();
+
+            if (emptyCells.Count == 0)
+            {
+                return;
+            }
+
+            var random = new Random();
+            (int row, int col) cell = emptyCells[random.Next(emptyCells.Count)];
+            int value = random.Next(10) == 0 ? 4 : 2;
+
+            _cells[cell.row, cell.col] = value;
+        }
+
+        public void PrintBoard()
+        {
+            string output = "";
+
+            for (int i = 0; i < _cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < _cells.GetLength(1); j++)
+                {
+                    output += _cells[i, j].ToString().PadLeft(5);
+                }
+
+                output += "\n";
+            }
+
+            UnityEngine.Debug.Log(output);
+        }
+    }
+}
