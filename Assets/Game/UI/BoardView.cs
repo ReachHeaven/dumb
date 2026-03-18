e607dab6858ea561cd8e5c834abb09d827b8b06e
Karim Amirov <karamirov@gmail.com>
2026-03-19 01:59:53 +0500

    Animations for merge, instantiate and popup

--- Assets/Game/UI/BoardView.cs
+++ Assets/Game/UI/BoardView.cs
@@ -1,4 +1,6 @@
-﻿using Game.Data;
+﻿using System.Collections;
+using Game.Data;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,8 +8,15 @@ namespace Game.UI
 {
     public class BoardView : MonoBehaviour
     {
-        [FormerlySerializedAs("_cells")] [SerializeField] private CellView[] cells;
-        [FormerlySerializedAs("_boardData")] [SerializeField] private BoardData boardData;
+        [FormerlySerializedAs("_cells")] [SerializeField]
+        private CellView[] cells;
+
+        [FormerlySerializedAs("_boardData")] [SerializeField]
+        private BoardData boardData;
+
+        [SerializeField] private TMP_Text popupPrefab;
+        [SerializeField] private Canvas canvas;
+        private int[] _previousBoard = new int[16];
 
         /// <summary>
         /// UpdateBoard by BoardData
@@ -17,8 +26,53 @@ namespace Game.UI
             int[] board = boardData.Board;
             for (int i = 0; i < cells.Length; i++)
             {
+                int oldValue = _previousBoard[i];
+                int newValue = board[i];
+
                 cells[i].SetValue(board[i]);
+
+                if (newValue != 0 && oldValue == 0)
+                {
+                    cells[i].PlayAppearAnimation();
+                }
+
+                else if (newValue != 0 && newValue > oldValue)
+                {
+                    cells[i].PlayMergeAnimation();
+                    ShowScorePopup(newValue, cells[i].transform.position);
+                }
             }
+
+            board.CopyTo(_previousBoard, 0);
+        }
+
+
+        private void ShowScorePopup(int value, Vector3 position)
+        {
+            TMP_Text popup = Instantiate(popupPrefab, position, Quaternion.identity, canvas.transform);
+            popup.text = $"+{value}";
+            StartCoroutine(TextAnimation(popup));
+        }
+
+        private IEnumerator TextAnimation(TMP_Text text)
+        {
+            float duration = 0.5f;
+            float elapsed = 0f;
+
+            Vector3 start = text.transform.position;
+            Color color = text.color;
+
+            while (elapsed < duration)
+            {
+                float t = elapsed / duration;
+                text.transform.position = start + Vector3.up * (t * 50f);
+                color.a = 1f - t;
+                text.color = color;
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            Destroy(text.gameObject);
         }
     }
 }
--- Assets/Game/UI/CellView.cs
+++ Assets/Game/UI/CellView.cs
@@ -1,6 +1,8 @@
-﻿using Game.Data;
+﻿using System.Collections;
+using Game.Data;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Game.UI
@@ -29,6 +31,58 @@ namespace Game.UI
                 valueText.gameObject.SetActive(true);
                 valueText.text = value == 0 ? "" : value.ToString();
             }
+        }
+
+        public void PlayMergeAnimation()
+        {
+            StartCoroutine(MergeAnimation());
+        }
+
+
+        private IEnumerator MergeAnimation()
+        {
+            Vector3 original = Vector3.one;
+            Vector3 big = original * 1.2f;
+            float duration = 0.1f;
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                transform.localScale = Vector3.Lerp(original, big, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                transform.localScale = Vector3.Lerp(big, original, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.localScale = original;
+        }
+
+        public void PlayAppearAnimation()
+        {
+            StartCoroutine(StartScale());
+        }
+
+        private IEnumerator StartScale()
+        {
+            float elapsed = 0f;
+            float duration = 0.1f;
+
+            while (elapsed < duration)
+            {
+                transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.localScale = Vector3.one;
         }
     }
 }
