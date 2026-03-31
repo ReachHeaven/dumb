785d64c0853b713e25f9371f00e032cfdf4f89a1
Karim Amirov <karamirov@gmail.com>
2026-03-31 20:44:41 +0500

    Audio was added

---
+++ Assets/Game/Core/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] private AudioSource musicSource;
+        [SerializeField] private AudioSource sfxSource;
+        [SerializeField] private AudioClip backgroundMusic;
+        [SerializeField] private AudioClip swipeSfx;
+        [SerializeField] private AudioClip gameOverSfx;
+
+        private void Start()
+        {
+            musicSource.clip = backgroundMusic;
+            musicSource.Play();
+        }
+
+        public void PlaySwipe()
+        {
+            sfxSource.PlayOneShot(swipeSfx);
+        }
+
+        public void PlayGameOver()
+        {
+            sfxSource.PlayOneShot(gameOverSfx);
+        }
+    }
+}
